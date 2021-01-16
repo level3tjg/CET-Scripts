7943de5a7fd378253a216716452fe3c155c6b367
level3tjg <t.grantham14@gmail.com>
2021-01-15 20:26:58 -0500

    readable

--- mods/noclip/init.lua
+++ mods/noclip/init.lua
@@ -15,49 +15,49 @@ registerForEvent("onUpdate", function()
     if (ImGui.IsKeyDown(16)) then
       speed = speed * 2
     elseif (ImGui.IsKeyDown(string.byte('Z'))) then
-      speed = speed / 3
+      speed = speed / 2
     end
     local x = position['x']
     local y = position['y']
     local z = position['z']
     if (ImGui.IsKeyDown(string.byte('A'))) then
-      Noclip.yaw = Noclip.yaw + speed*2
+      Noclip.yaw = Noclip.yaw + speed * 2
     end
     if (ImGui.IsKeyDown(string.byte('D'))) then
-      Noclip.yaw = Noclip.yaw - speed*2
+      Noclip.yaw = Noclip.yaw - speed * 2
     end
     local yaw = Noclip.yaw % 360
-    local x_mod = speed * math.tan(yaw % 90 / 100)
-    local y_mod = speed * math.tan((90 - yaw % 90) / 100)
-    local quad = math.floor(yaw /90) + 1
+    local a1 = speed * math.tan(yaw % 90 / 100)
+    local a2 = speed * math.tan((90 - yaw % 90) / 100)
+    local quad = math.floor(yaw / 90) + 1
     if (ImGui.IsKeyDown(string.byte('W'))) then
       if (quad == 1) then
-        y = y + y_mod
-        x = x - x_mod
+        y = y + a2
+        x = x - a1
       elseif (quad == 2) then
-        y = y - x_mod
-        x = x - y_mod
+        y = y - a1
+        x = x - a2
       elseif (quad == 3) then
-        y = y - y_mod
-        x = x + x_mod
+        y = y - a2
+        x = x + a1
       elseif (quad == 4) then
-        y = y + x_mod
-        x = x + y_mod
+        y = y + a1
+        x = x + a2
       end
     end
     if (ImGui.IsKeyDown(string.byte('S'))) then
       if (quad == 1) then
-        y = y - y_mod
-        x = x + x_mod
+        y = y - a2
+        x = x + a1
       elseif (quad == 2) then
-        y = y + x_mod
-        x = x + y_mod
+        y = y + a1
+        x = x + a2
       elseif (quad == 3) then
-        y = y + y_mod
-        x = x - x_mod
+        y = y + a2
+        x = x - a1
       elseif (quad == 4) then
-        y = y - x_mod
-        x = x - y_mod
+        y = y - a1
+        x = x - a2
       end
     end
     if (ImGui.IsKeyDown(17)) then
